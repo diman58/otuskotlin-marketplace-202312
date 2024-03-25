3149083d09cc744ade59231a24b368a9687edb28
Dmitrii_Tsarev <dcivicedge@gmail.com>
2024-03-25 21:11:59 +0300

    final

--- hub/common/src/main/kotlin/models/ExchangeOfferAmount.kt
+++
@@ -1,8 +0,0 @@
-package models
-
-class ExchangeOfferAmount(private val amount: String) {
-    fun asString() = amount
-    companion object {
-        val NONE = ExchangeOfferAmount("")
-    }
-}
--- hub/common/src/main/kotlin/models/ExchangeOfferCurrency.kt
+++
@@ -1,8 +0,0 @@
-package models
-
-data class ExchangeOfferCurrency(private val name: String) {
-    fun asString() = name
-    companion object {
-        val NONE = ExchangeOfferCurrency("")
-    }
-}
